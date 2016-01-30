2abbd217c5f10cca68985aafd1489e19e914a291
kamranrana <kamkr19@vt.edu>
2016-01-30 18:22:36 -0500

    Translucent Status Bar

--- ShahmirKam/app/src/main/java/com/example/kamran/shahmirkam/MainActivity.java
+++ ShahmirKam/app/src/main/java/com/example/kamran/shahmirkam/MainActivity.java
@@ -17,11 +17,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 }
+
