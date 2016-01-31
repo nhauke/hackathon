529193ca224b823e8dcbcd216d1644c9b39a65ae
Shahmir Ahmed <shahmirahmed97@gmail.com>
2016-01-31 09:18:19 -0500

    Main with Randomize

--- ShahmirKam/app/src/main/java/com/example/kamran/shahmirkam/MainActivity.java
+++ ShahmirKam/app/src/main/java/com/example/kamran/shahmirkam/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,23 +25,31 @@ public class MainActivity extends AppCompatActivity {
 
         int index = 0;
         int val = 30;
-        ArrayList<Integer> indexes = new ArrayList<Integer>();
-        /* ArrayList<String> quotes = new ArrayList<String>();
+        //ArrayList<Integer> indexes = new ArrayList<Integer>();
+        //ArrayList<String> quotes1 = new ArrayList<String>();
+        /*
         //File f = new File("C:\\Users\\Kamran\\Documents\\Android App\\hackathon\\hackathon\\Motivational Quotes.txt");
         File f = new File("com/example/kamran/shahmirkam/Motivational Quotes.txt");
+        x
+        String file = "raw/motivational_quotes.txt"; // res/raw/test.txt also work.
+        InputStream in = this.getClass().getClassLoader().getResourceAsStream(file);
         Scanner file1 = null;
         try {
-            file1 = new Scanner(f);
+            file1 = new Scanner(in);
         } catch (Exception e) {
             System.out.println(e);
             System.out.println("could not find file");
+            Log.e("Tag", "could not find file");
         }
-        for (int i = 0; i < 30; i++) {
+        /*for (int i = 0; i < 30; i++) {
             indexes.add(i);
         }
         while (file1.hasNextLine()) {
-            quotes.add(file1.nextLine() + "\n");
-        }*/
+            quotes1.add(file1.nextLine());
+        }
+        //File f = new File(getResources().getAssets();
+        */
+        index = (int) (Math.random() * val);
         String[] text_quotes = {
         "\"Go for it now. The future is promised to no one\"",
         "\"The more weird you are is a reflection of how committed you are to focusing on your goal\"",
